10dc8277291c7f2c243cd672f1f6fde533930c6a
Dimitrii <sexykdi@gmail.com>
2023-11-27 14:50:23 +0300

    display for modelformat

--- src/model_format.rs
+++ src/model_format.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 /// Enum that should be used for constructing object detection model
 /// 
 /// Basic usage:
@@ -6,11 +8,20 @@
 /// let mf = ModelFormat::Darknet;
 /// ```
 /// 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone, Copy)]
 pub enum ModelFormat {
     // Most of time use Darknet specification when you are using either https://github.com/AlexeyAB/darknet or https://github.com/pjreddie/darknet to train neural network of version v3, v4 or v7 (classic ones).
     Darknet,
     // When you are using other DL/ML/NN framework than Darknet one it is better to convert weights to ONNX specification and use it further
     // It is better to converte weights to ONNX specification with providing flag `opset = 12`` (So far as I tested it do not show any runtime errors) - e.g. YOLOv8 - https://github.com/ultralytics/ultralytics/issues/1097
     ONNX,
+}
+
+impl fmt::Display for ModelFormat {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ModelFormat::Darknet => write!(f, "darknet"),
+            ModelFormat::ONNX => write!(f, "onnx"),
+        }
+    }
 }
